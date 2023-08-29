efccf1e435cc8e300ca3ac862ebbc3f1f9892cc8
rishavvrathod <96969922+rishavvrathod@users.noreply.github.com>
2023-08-29 21:44:37 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,15 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const BodyParser = require('body-parser');
+const DNS = require('dns');
+
+// configuring body-parser for POST methods
+app.use(BodyParser.urlencoded(
+  {
+    extended: false
+  }
+));
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
@@ -10,15 +19,109 @@ app.use(cors());
 
 app.use('/public', express.static(`${process.cwd()}/public`));
 
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-// Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
+// array of object to map links with an ID
+const URLs = [];
+
+// ID variable to be mapped with
+let id = 0;
+
+// post request to create the shortened url
+app.post('/api/shorturl', (req, res) => {
+  const { url: _url } = req.body;
+
+  if (_url === "") {
+    return res.json(
+      {
+        "error": "invalid url"
+      }
+    );
+  }
+
+  let parsed_url;
+  const modified_url = _url.replace(/(ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/, '');
+
+  try {
+    parsed_url = new URL(_url);
+  }
+  catch (err) {
+    return res.json(
+      {
+        "error": "invalid url"
+      }
+    );
+  }
+
+  DNS.lookup(modified_url, (err) => {
+    if (err) {
+      return res.json(
+        {
+          "error": "invalid url"
+        }
+      );
+    }
+    else {
+      const link_exists = URLs.find(l => l.original_url === _url)
+
+      if (link_exists) {
+        return res.json(
+          {
+            "original_url": _url,
+            "short_url": id
+          }
+        );
+      }
+      else {
+        // increment for each new valid url
+        ++id;
+
+        // object creation for entry into url
+        const url_object = {
+          "original_url": _url,
+          "short_url": `${id}`
+        };
+
+        // pushing each new entry into the array
+        URLs.push(url_object);
+
+        // return the new entry created
+        return res.json(
+          {
+            "original_url": _url,
+            "short_url": id
+          }
+        );
+      }
+    }
+  });
 });
 
-app.listen(port, function() {
+// get request to navigate to the url
+app.get('/api/shorturl/:id', (req, res) => {
+  const { id: _id } = req.params;
+
+  // finding if the id already exists
+  const short_link = URLs.find(sl => sl.short_url === _id);
+
+  if (short_link) {
+    return res.redirect(short_link.original_url);
+  }
+  else {
+    return res.json(
+      {
+        "error": "invalid URL"
+      }
+    );
+  }
+});
+
+app.listen(port, function () {
   console.log(`Listening on port ${port}`);
 });
+
+// app.listen(3000, () => {
+//   console.log("Server Running....");
+// });
